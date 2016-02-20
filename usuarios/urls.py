fad932da63e561916119dccab9887ef7afaab229
lucho <lhitaliani@gmail.com>
2016-02-20 07:03:33 -0300

    Creacion de usuario muy basico

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^crear/$', views.crear_usuario, name='crear_usuario'),
+    url(r'^lista/$', views.lista_usuario, name='lista_usuario'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,29 @@
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 
 def index(request):
-
     if request.method == 'POST':
         respuesta = request.POST.get('algo','')
         return render(request,'HTML/resp-index.html',{'respuesta':respuesta})
     else:
         return render(request,'HTML/index.html')
 
-    return render(request,'HTML/index.html')    
+    return render(request,'HTML/index.html')
+
+from django.contrib.auth.models import User
+
+def crear_usuario(request):
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario','')
+        clave = request.POST.get('clave','')
+
+        user = User.objects.create_user(usuario,'',clave)
+
+        return HttpResponseRedirect('/usuario/lista/')
+    else:
+        return render(request, 'HTML/creacion-usuario.html')
+
+def lista_usuario(request):
+    mostrar_usuarios = User.objects.all()
+    return render(request, 'HTML/lista.html', {'usuario':mostrar_usuarios})
