a6d3fb0427587db1e4ad5b8ae806b42ee3f3e657
lucho <lhitaliani@gmail.com>
2016-02-21 09:42:26 -0300

    Se agregaron dos errores al creador de usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.http import HttpResponse
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
@@ -17,10 +18,20 @@ def crear_usuario(request):
     if request.method == 'POST':
         usuario = request.POST.get('usuario','')
         clave = request.POST.get('clave','')
+        clave2 = request.POST.get('clave2','')
 
-        user = User.objects.create_user(usuario,'',clave)
+        if User.objects.get(username=usuario):
+            error_nombre = 'El nombre ya existe'
+            return render(request, 'HTML/creacion-usuario.html',
+                            {'error_nombre' : error_nombre})
 
-        return HttpResponseRedirect('/usuario/lista/')
+        if clave == clave2:
+            user = User.objects.create_user(usuario,'',clave)
+            return HttpResponseRedirect('/usuario/lista/')
+        else:
+            error_clave = 'Las contraseñas son distintas'
+            return render(request, 'HTML/creacion-usuario.html',
+                            {'error_clave' : error_clave})
     else:
         return render(request, 'HTML/creacion-usuario.html')
 
