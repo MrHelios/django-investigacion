2b13acaf139fd26a964e4ef4c7402e1b2db70ea9
lucho <lhitaliani@gmail.com>
2016-02-22 19:56:43 -0300

    Agregue servicio de login y logout basico

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-
+    # Sistema de creacion de cuenta
     url(r'^crear/$', views.crear_usuario, name='crear_usuario'),
     url(r'^crear-user-form/$', views.crear_usuario_FORM, name='crear_usuario_Form'),
-
+    # Sistema de login
+    url(r'^login/$', views.sistema_login, name='sistema_login'),
+    url(r'^logout/$', views.sistema_logout, name='sistema_logout'),
+    # Otros
     url(r'^lista/$', views.lista_usuario, name='lista_usuario'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,15 +1,19 @@
 from django import forms
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, render_to_response
 
 def index(request):
+
+    if request.user.is_authenticated():
+        usuario = request.user
+    else:
+        usuario = 'Anonimo'    
+
     if request.method == 'POST':
         respuesta = request.POST.get('algo','')
-        return render(request,'HTML/resp-index.html',{'respuesta':respuesta})
+        return render(request,'HTML/resp-index.html', {'respuesta':respuesta, 'usuario': usuario})
     else:
-        return render(request,'HTML/index.html')
-
-    return render(request,'HTML/index.html')
+        return render(request,'HTML/index.html', {'usuario': usuario})
 
 from django.contrib.auth.models import User
 
@@ -47,13 +51,37 @@ def crear_usuario_FORM(request):
         if form.is_valid():
             user = User.objects.create_user(
                 form.cleaned_data['usuario'],
-                form.cleaned_data['clave2'],
                 form.cleaned_data['email'],
+                form.cleaned_data['clave2'],
             )
             return HttpResponseRedirect('/usuario/lista/')
-        else:            
+        else:
             variable = RequestContext(request, {'form' : form})
     else:
         form = Registrar_form()
         variable = RequestContext(request, {'form' : form})
     return render_to_response('HTML/crear-user-form.html', variable)
+
+from django.contrib.auth import authenticate, login
+
+def sistema_login(request):
+    form = Registrar_form()
+    if request.method == 'POST':
+        usuario = request.POST['usuario']
+        clave = request.POST['clave']
+        user = authenticate(username=usuario, password=clave)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/usuario/')
+        else:
+            errors = True
+            variable = RequestContext(request, { 'form': form, 'errors': errors })
+    else:
+        variable = RequestContext(request, { 'form': form })
+    return render_to_response('HTML/login.html', variable)
+
+from django.contrib.auth import logout
+
+def sistema_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/usuario/')
